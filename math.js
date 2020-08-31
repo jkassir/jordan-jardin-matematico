c3f2236bc35ab154a49fd51fc26397f2fb744da0
jkassir <70544735+jkassir@users.noreply.github.com>
2020-08-31 17:42:03 -0300

    Adding initial project Files

---
+++ math.js
@@ -0,0 +1,42 @@
+var answer;
+var score=0;
+var backgroundImages=[];
+
+function nextQuestion() {
+    const n1=Math.floor(Math.random()*5);
+    document.getElementById('n1').innerHTML=n1;
+    const n2=Math.floor(Math.random()*6);
+    document.getElementById('n2').innerHTML=n2;
+    answer=n1+n2;
+}
+
+function checkAnswer() {
+    const prediction=predictImage();
+    console.log("Prediction "+prediction);
+    console.log(`Answer ${answer}`);
+
+    if(prediction==answer){
+        score++;
+        if (score<=6){
+            backgroundImages.push(`url('images/background${score}.svg')`);
+            document.body.style.backgroundImage=backgroundImages; 
+        }else{
+            alert("Well Donde!!! your math garden is in full bloom. Want to start again?")
+            score=0;
+            backgroundImages=[];
+            document.body.style.backgroundImage=backgroundImages; 
+ 
+        }
+        
+        console.log(`Correct! Score: ${score}`);
+    } else {
+        if (score!=0) {score--}
+        console.log(`Wrong! Score: ${score}`);
+        alert('Ooops! Check your calculations and try the number neater next time.');
+        setTimeout(function(){
+            backgroundImages.pop();
+            document.body.style.backgroundImage=backgroundImages; 
+        },1000);
+
+    }
+}
